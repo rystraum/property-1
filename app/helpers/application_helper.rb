a9d69960823e93cb8bcddd0669106d223afc8866
Gavin Hughes <gavhug@gmail.com>
2011-08-25 15:05:16 +0800

    Add <body> class containing :controller and :action names.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  
+  # Assigned to <body> for scoping.
+  def body_class
+    [request.path_parameters[:controller], request.path_parameters[:action]].map(&:parameterize).join('--')
+  end
 
   def m2
     "m<sup>2</sup>".html_safe
